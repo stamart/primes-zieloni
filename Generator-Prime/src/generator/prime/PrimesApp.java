6b0266deedbd7c97a764ef29c2141562ee3dfa8c
Staszek Martowicz <stamart@gmail.com>
2021-02-24 15:41:25 +0100

    ocena zadania 1

--- Generator-Prime/src/generator/prime/PrimesApp.java
+++ Generator-Prime/src/generator/prime/PrimesApp.java
@@ -9,7 +9,7 @@ public class PrimesApp {
 
     public static void main(String[] args) {
         GeneratePrimes generatePrimes = new GeneratePrimes();
-        List<Integer> primes = generatePrimes.findPrimes(100);
+        List<Integer> primes = generatePrimes.findPrimes(120);
 
         ShowPrimes.show_primes(primes);
     }
