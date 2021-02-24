0a73ca31a576b2d9a3b81608e1205e1fbf99767e
Staszek Martowicz <stamart@gmail.com>
2021-02-24 15:23:51 +0100

    dodanie gitignore

--- Generator-Prime/src/generator/prime/calculation/GeneratePrimes.java
+++ Generator-Prime/src/generator/prime/calculation/GeneratePrimes.java
@@ -1,12 +1,22 @@
 
 package generator.prime.calculation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GeneratePrimes {
     
     public static List<Integer> find_primes(){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        primes.add(3);
+        primes.add(5);
+        primes.add(7);
+        primes.add(11);
+        primes.add(13);
+        primes.add(11);
+        primes.add(11);
+        return  primes;
     }
     
 }
