213b143d868b9701426f4edcb0733062b473f104
Staszek Martowicz <stamart@gmail.com>
2021-03-01 14:59:10 +0100

    gitigonre

--- Generator-Prime/src/generator/prime/PrimesApp.java
+++ Generator-Prime/src/generator/prime/PrimesApp.java
@@ -9,7 +9,13 @@ public class PrimesApp {
 
     public static void main(String[] args) {
         List<Integer> primes = GeneratePrimes.find_primes();
-        ShowPrimes.show_primes(primes);
+//        ShowPrimes.show_primes(primes);
+        
+        
+        ShowPrimes.show_primes_in_columns(primes, 240, 30, ' ','*',false);
+        System.out.println("");
+        ShowPrimes.show_primes_in_columns(primes, 240, 6,' ','*',false); 
+        
     }
     
 }
