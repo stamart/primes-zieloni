1fd8c867973868c4a754a339a6cbb20f123d05b2
Staszek Martowicz <stamart@gmail.com>
2021-03-01 15:07:42 +0100

    zmiany w main

--- Generator-Prime/src/generator/prime/PrimesApp.java
+++ Generator-Prime/src/generator/prime/PrimesApp.java
@@ -11,11 +11,11 @@ public class PrimesApp {
         GeneratePrimes generatePrimes = new GeneratePrimes();
       
         
-        List<Integer> primes = generatePrimes.findPrimes(120);
+        List<Integer> primes = generatePrimes.findPrimes(240);
 
-        ShowPrimes.show_primes_in_columns(primes, 240, 30, ' ','*',false);
+        ShowPrimes.show_primes_in_columns(primes, 240, 30, ' ','*',true);
         System.out.println("");
-        ShowPrimes.show_primes_in_columns(primes, 240, 6,' ','*',false); 
+        ShowPrimes.show_primes_in_columns(primes, 240, 6,' ','*',true); 
         
     }
     
